90248c611845cce2f906586e08fa693497038ed9
Paulo McNally <paulomcnally@gmail.com>
2020-09-09 22:02:09 -0600

    Add test on user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has factory' do
+    expect(create(:user)).to be_persisted  
+  end
 end
